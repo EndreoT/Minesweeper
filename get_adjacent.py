6998464fd3a5b26a8dca0ca16d381bda9ff4cffb
EndreoT <endreotm@gmail.com>
2019-03-10 22:14:48 -0700

    overhauling minesweeper. Adding in cell entry objects

--- get_adjacent.py
+++ get_adjacent.py
@@ -6,8 +6,8 @@ def get_adjacent(index: Tuple[int, int]) -> Set[Tuple[int, int]]:
 
     x, y = index
 
-    return {
+    return (
         (x - 1, y - 1), (x, y - 1), (x + 1, y - 1),
         (x - 1, y),                 (x + 1, y),
-        (x - 1, y + 1), (x, y + 1), (x + 1, y + 1),
-        }
+        (x - 1, y + 1), (x, y + 1), (x + 1, y + 1)
+    )
